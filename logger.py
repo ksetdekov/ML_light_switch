0c799426298c3ec113e3c52cb3a94a429da623af
ksetdekov <ksetdekov@gmail.com>
2020-02-04 11:02:50 +0300

    Update logger.py
    this should return nan as test if communication issue

--- logger.py
+++ logger.py
@@ -26,14 +26,26 @@ def graph(status1):
     plt.scatter(x,y)
     plt.plot(x,y)
     plt.draw()
-    
+
+Goodvalues = ['TRUE', 'FALSE']
+
+def responseformat(resp):
+    if resp in Goodvalues:
+        return resp
+    else:
+        return "nan"
+
 while True:
     result1 = subprocess.run(['miplug', '--ip', "192.168.1.2", '--token', "e1ef4f9f97aaf257f54270adf2d998f3", 'status'], stdout=subprocess.PIPE)
     txt1=result1.stdout.decode("utf-8")
     status1 = txt1.split()[1]
+    status1 = responseformat(status1)
+    
     result2 = subprocess.run(['miplug', '--ip', "192.168.1.3", '--token', "9a959331e150c3a6919df3433952938e", 'status'], stdout=subprocess.PIPE)
     txt2=result2.stdout.decode("utf-8")
-    status2 = txt2.split()[1]    
+    status2 = txt2.split()[1]
+    status1 = responseformat(status1)
+    
     write_status(status1,status2)
     #graph(status1)
     plt.pause(60)
