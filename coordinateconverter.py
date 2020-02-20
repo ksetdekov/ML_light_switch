d08c12b1fe8082152c670419acd61802b2d0c82f
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-20 11:54:03 +0300

    created distance to home calculator for everyone

--- coordinateconverter.py
+++ coordinateconverter.py
@@ -1,10 +1,7 @@
+from spatial import vincenty_inverse as vi
+
 with open("coordinates.txt", "r") as f:
     data = f.readlines()
-print(data[2])
-print(data[3])
-
-
-from spatial import vincenty_inverse as vi
-home = [55.711295, 37.612321]
-p1 = [-81.4061265, 41.250386]
+home = [float(data[1].split(":", 1)[1]), float(data[2].split(":", 1)[1])]
+p1 = [55.5993645, 37.2696333]
 print(vi(home, p1).km())
--- getlocation.py
+++ getlocation.py
@@ -1,6 +1,16 @@
 from locationsharinglib import Service
+from spatial import vincenty_inverse as vi
+
+with open("coordinates.txt", "r") as f:
+    data = f.readlines()
+home = [float(data[1].split(":", 1)[1]), float(data[2].split(":", 1)[1])]
+p1 = [55.5993645, 37.2696333]
+print(vi(home, p1).km())
+
 cookies_file = 'cookies.txt'
 google_email = 'ksetdekov@gmail.com'
 service = Service(cookies_file=cookies_file, authenticating_account=google_email)
 for person in service.get_all_people():
     print(person)
+    pp = [person.latitude, person.longitude]
+    print(vi(home, pp).km())
