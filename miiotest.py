c45bcd794c08da2a70641c3dfc9b4059de28f670
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-20 14:45:34 +0300

    Update miiotest.py
    to try later

--- miiotest.py
+++ miiotest.py
@@ -1,5 +1,9 @@
 import miio
+from miio import ChuangmiPlug
+
 ip = '192.168.1.2'
 token = 'e1ef4f9f97aaf257f54270adf2d998f3'
-switch = miio.miplug.Miplug(ip, token)
-switch.on()
+plug = ChuangmiPlug(ip, token , model="chuangmi.plug.m3")
+print(plug.status())
+plug.off()
+plug.on()
