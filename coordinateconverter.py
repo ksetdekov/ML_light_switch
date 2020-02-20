7361cc0a20b5ad4b07d79a9f2bcd74f11f0574df
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-20 12:52:16 +0300

    deleted unneeded file, fixed dependencies changed getlocation to only output names and positions

--- coordinateconverter.py
+++
@@ -1,7 +0,0 @@
-from spatial import vincenty_inverse as vi
-
-with open("coordinates.txt", "r") as f:
-    data = f.readlines()
-home = [float(data[1].split(":", 1)[1]), float(data[2].split(":", 1)[1])]
-p1 = [55.5993645, 37.2696333]
-print(vi(home, p1).km())
