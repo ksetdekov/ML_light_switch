34c5e395f1e2e633f45598197b2a969e8c0d8920
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-20 12:50:14 +0300

    cleared up logger

--- logger.py
+++ logger.py
@@ -1,25 +1,28 @@
-from time import sleep, strftime, time
-import matplotlib.pyplot as plt
-import miio
 import subprocess
+from time import strftime, time
+import matplotlib.pyplot as plt
 
 plt.ion()
 x = []
 y = []
 
-def write_status(status1,status2):
-    with open("/home/pi/Documents/testing/status.csv", "a") as log:
-        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"),str(",".join([status1,status2]))))
 
-def graph(status1):
-    y.append(status1)
+def write_status(st1, st2):
+    with open("/home/pi/Documents/testing/status.csv", "a") as log:
+        log.write("{0},{1}\n".format(strftime("%Y-%m-%d %H:%M:%S"), str(",".join([st1, st2]))))
+
+
+def graph(variable1):
+    y.append(variable1)
     x.append(time())
     plt.clf()
-    plt.scatter(x,y)
-    plt.plot(x,y)
+    plt.scatter(x, y)
+    plt.plot(x, y)
     plt.draw()
 
+
 Goodvalues = ['True', 'False']
+
 
 def responseformat(resp):
     if resp in Goodvalues:
@@ -27,17 +30,20 @@ def responseformat(resp):
     else:
         return "nan"
 
+
 while True:
-    result1 = subprocess.run(['miplug', '--ip', "192.168.1.2", '--token', "e1ef4f9f97aaf257f54270adf2d998f3", 'status'], stdout=subprocess.PIPE)
-    txt1=result1.stdout.decode("utf-8")
+    result1 = subprocess.run(['miplug', '--ip', "192.168.1.2", '--token', "e1ef4f9f97aaf257f54270adf2d998f3", 'status'],
+                             stdout=subprocess.PIPE)
+    txt1 = result1.stdout.decode("utf-8")
     status1 = txt1.split()[1]
     status1 = responseformat(status1)
-    
-    result2 = subprocess.run(['miplug', '--ip', "192.168.1.3", '--token', "9a959331e150c3a6919df3433952938e", 'status'], stdout=subprocess.PIPE)
-    txt2=result2.stdout.decode("utf-8")
+
+    result2 = subprocess.run(['miplug', '--ip', "192.168.1.3", '--token', "9a959331e150c3a6919df3433952938e", 'status'],
+                             stdout=subprocess.PIPE)
+    txt2 = result2.stdout.decode("utf-8")
     status2 = txt2.split()[1]
     status1 = responseformat(status1)
-    
-    write_status(status1,status2)
-    #graph(status1)
+
+    write_status(status1, status2)
+    # graph(status1)
     plt.pause(60)
