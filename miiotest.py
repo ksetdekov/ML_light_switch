2718c766c57576312b3effd27bad157416cc76f1
ksetdekov <ksetdekov@gmail.com>
2020-02-22 16:55:22 +0300

    working with miioplug without cli - works

--- miiotest.py
+++ miiotest.py
@@ -1,9 +1,12 @@
-import miio
+import time
 from miio import ChuangmiPlug
 
 ip = '192.168.1.2'
 token = 'e1ef4f9f97aaf257f54270adf2d998f3'
 plug = ChuangmiPlug(ip, token , model="chuangmi.plug.m3")
 print(plug.status())
-plug.off()
 plug.on()
+print(plug.status())
+time.sleep(5)
+plug.off()
+print(plug.status())
