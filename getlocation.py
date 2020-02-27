8e5ff2702cc0c665e828dbf7a17213b63068a82c
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-27 10:42:35 +0300

    formatted as a function location getter

--- getlocation.py
+++ getlocation.py
@@ -1,27 +1,24 @@
-from locationsharinglib import Service
-from spatial import vincenty_inverse as vi
-
-with open("coordinates.txt", "r") as f:
-    data = f.readlines()
-home = [float(data[1].split(":", 1)[1]), float(data[2].split(":", 1)[1])]
-
-cookies_file = 'cookies.txt'
-google_email = 'ksetdekov@gmail.com'
-service = Service(cookies_file=cookies_file, authenticating_account=google_email)
-names = []
-distances = []
-for person in service.get_all_people():
-    names.append(person.nickname)
-    pp = [person.latitude, person.longitude]
-    distances.append(vi(home, pp).km())
-dictionary = dict(zip(names, distances))
-
-sortednames = []
-sorteddists = []
-for elem in sorted(dictionary.items()):
-    sortednames.append(elem[0])
-    sorteddists.append(str(elem[1]))
-
-allnames = str(",".join(sortednames))
-alldistances = str(",".join(sorteddists))
-print(str(",".join([alldistances, allnames])))
+def string_distances():
+    from locationsharinglib import Service
+    from spatial import vincenty_inverse as vi
+    with open("coordinates.txt", "r") as f:
+        data = f.readlines()
+    home = [float(data[1].split(":", 1)[1]), float(data[2].split(":", 1)[1])]
+    cookies_file = 'cookies.txt'
+    google_email = 'ksetdekov@gmail.com'
+    service = Service(cookies_file=cookies_file, authenticating_account=google_email)
+    names = []
+    distances = []
+    for person in service.get_all_people():
+        names.append(person.nickname)
+        pp = [person.latitude, person.longitude]
+        distances.append(vi(home, pp).km())
+    dictionary = dict(zip(names, distances))
+    sortednames = []
+    sorteddists = []
+    for elem in sorted(dictionary.items()):
+        sortednames.append(elem[0])
+        sorteddists.append(str(elem[1]))
+    allnames = str(",".join(sortednames))
+    alldistances = str(",".join(sorteddists))
+    return str(",".join([alldistances, allnames]))
