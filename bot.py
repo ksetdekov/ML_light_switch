7d9f0b5922f1f10939d2b9a01e5c160734ccd00f
Kirill Setdekov <ksetdekov@gmail.com>
2020-03-22 15:13:41 +0300

    something done

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import telebot
 from telebot import apihelper
 
-ip = '94.102.52.30'
-port = '1080'
+ip = '51.158.114.177'
+port = '8811'
 
 apihelper.proxy = {
     'https': 'socks5h://{}:{}'.format(ip, port)
