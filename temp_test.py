90e04abe5f6404fb58118ca8760880f77f29bcb8
ksetdekov <ksetdekov@gmail.com>
2020-10-14 00:11:51 +0300

    started db use

---
+++ temp_test.py
@@ -0,0 +1,25 @@
+import datetime
+import sqlite3
+
+import Adafruit_DHT
+
+
+def add_data(temp, hum, curtime):
+    try:
+        sqlite_con = sqlite3.connect('temperature.db')
+        cursor = sqlite_con.cursor()
+        sqlite_insert = """INSERT INTO 'timelog'
+                ('stamp', 'temp', 'hum')
+                VALUES (?, ?, ?);"""
+        data_tuple = (curtime, temp, hum)
+        cursor.execute(sqlite_insert, data_tuple)
+        sqlite_con.commit()
+    except sqlite3.Error as error:
+        print("Error while working with sqlite", error)
+
+
+sensor = Adafruit_DHT.DHT11
+while True:
+    hum, temp = Adafruit_DHT.read_retry(sensor, 4)
+    print('Temp: {} C humidity: {}'.format(temp, hum))
+    add_data(temp, hum, datetime.datetime.now())
