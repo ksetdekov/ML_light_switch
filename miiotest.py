2dbd138a4e9eebacdabfac8301f76fb435dec7ce
ksetdekov <ksetdekov@gmail.com>
2020-10-14 00:33:35 +0300

    changed logger.py

--- miiotest.py
+++ miiotest.py
@@ -3,15 +3,15 @@ import time
 from miio import ChuangmiPlug
 
 # plug
-# ip = '192.168.1.2'
-# token = 'e1ef4f9f97aaf257f54270adf2d998f3'
-# plug = ChuangmiPlug(ip, token, model="chuangmi.plug.m3")
-# print(plug.status().is_on)
-# plug.on()
-# print(plug.status().is_on)
-# time.sleep(5)
-# plug.off()
-# print(plug.status().is_on)
+ip = '192.168.1.2'
+token = 'e1ef4f9f97aaf257f54270adf2d998f3'
+plug = ChuangmiPlug(ip, token, model="chuangmi.plug.m3")
+print(plug.status().is_on)
+plug.on()
+print(plug.status().is_on)
+time.sleep(5)
+plug.off()
+print(plug.status().is_on)
 
 # heater
 ip = '192.168.1.26'
