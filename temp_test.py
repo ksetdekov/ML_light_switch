96568a134d7f34e4fc0aed0aa982eca9232b32c1
ksetdekov <ksetdekov@gmail.com>
2020-10-14 00:33:16 +0300

    expanded temp_test.py

--- temp_test.py
+++ temp_test.py
@@ -4,14 +4,14 @@ import sqlite3
 import Adafruit_DHT
 
 
-def add_data(temp, hum, curtime):
+def add_data(temp_inp, hum_inp, cur_time):
     try:
         sqlite_con = sqlite3.connect('temperature.db')
         cursor = sqlite_con.cursor()
         sqlite_insert = """INSERT INTO 'timelog'
                 ('stamp', 'temp', 'hum')
-                VALUES (?, ?, ?);"""
-        data_tuple = (curtime, temp, hum)
+                VALUES (?, ?, ?, ?, ?);"""
+        data_tuple = (cur_time, temp_inp, hum_inp)
         cursor.execute(sqlite_insert, data_tuple)
         sqlite_con.commit()
     except sqlite3.Error as error:
